ca2e2d7eb0cf5d88720ecaa660da89a395e1cddf
Vandame <vandamedeveloper@gmail.com>
2023-09-02 17:02:07 +0200

    feature(UI): added ui manager script and created GameManager gameObject in the scene

---
+++ Assets/Resources/Scripts/GameUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject restartButton;
+
+    [Header("Knife Count Display")]
+    [SerializeField]
+    private GameObject panelKnifes;
+    [SerializeField]
+    private GameObject iconKnife;
+    [SerializeField]
+    private Color usedKnifeIconColor;
+
+    private void DisplayRestartButton(){
+        restartButton.SetActive(true);
+    }
+
+    private void SetInitialDisplayKnifeCount(int count){
+        for (int i = 0; i < count; i++)
+        {
+            Instantiate(iconKnife, panelKnifes.transform);
+        }
+    }
+
+    private int KnifeIconIndexToChange = 0;
+
+    public void DecrementDisplayKnifeCount(){
+        panelKnifes.transform.GetChild(KnifeIconIndexToChange).GetComponent<Image>().color = usedKnifeIconColor;
+        KnifeIconIndexToChange++;
+    }
+
+}
